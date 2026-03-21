d071b97e6bd623c829a6b511944f79aa59a6b594
Sanjay-D-works <sanjaydevarajworks@gmail.com>
2026-03-21 12:21:28 +0530

    Implement face data loading and webcam capture

--- Smart-Attendance-System-using-Face-Recognition-main/AttendanceSystem.py
+++ Smart-Attendance-System-using-Face-Recognition-main/AttendanceSystem.py
@@ -4,5 +4,16 @@ import os
 import face_recognition
 import datetime
 
+known_face = []
+known_name = []
 
+for filename in os.listdir('image'):
+    image = face_recognition.load_image_file(os.path.join('image', filename))
+    encoding = face_recognition.face_encodings(image)[0]
+    known_face.append(encoding)
+    known_name.append(os.path.splitext(filename)[0])
 
+video_capture = cv2.VideoCapture(0)
+
+attendance_marked = False
+- Implement face data loading and webcam capture
